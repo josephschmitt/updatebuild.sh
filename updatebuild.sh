95758616c7e0227ebb96cce3d98468a575ddcb7d
Joseph Schmitt <me@joe.sh>
2013-09-03 12:10:56 -0700

    Empty buildnumber file. Removed hard-coded plist location

--- updatebuild.sh
+++ updatebuild.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # Sample usage: ./updatebuild.sh path/to/xcode/workspace build/file.txt 
 
-WORKSPACE=$1
+PLIST=$1
 FILE=$2
 LAST_BUILD="`cat $FILE`"
 BUILD_NUMBER=$((LAST_BUILD+1))
 
 # Update build number in Xcode project
-/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $BUILD_NUMBER" "$WORKSPACE/Kiddo/Kiddo-Info.plist"
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $BUILD_NUMBER" "$PLIST"
 
 # Save new build number into file
 echo $BUILD_NUMBER > $FILE
